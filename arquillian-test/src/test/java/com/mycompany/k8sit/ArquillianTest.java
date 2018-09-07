0258642631d1f5032b7881473242e8bb14637bf3
bszeti <bszeti@redhat.com>
2018-09-07 01:03:39 -0400

    Add shutDownMariadb test

--- arquillian-test/src/test/java/com/mycompany/k8sit/ArquillianTest.java
+++ arquillian-test/src/test/java/com/mycompany/k8sit/ArquillianTest.java
@@ -1,6 +1,8 @@
 package com.mycompany.k8sit;
 
 import com.jayway.jsonpath.JsonPath;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.v4_0.PodList;
 import io.fabric8.kubernetes.api.model.v4_0.ServiceList;
 import io.fabric8.kubernetes.client.dsl.ExecListener;
@@ -14,6 +16,7 @@ import org.arquillian.cube.openshift.impl.client.OpenShiftAssistant;
 import org.arquillian.cube.openshift.impl.enricher.RouteURL;
 import org.arquillian.cube.openshift.impl.requirement.RequiresOpenshift;
 import org.arquillian.cube.requirement.ArquillianConditionalRunner;
+import org.awaitility.Awaitility;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -33,6 +36,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
@@ -65,6 +69,11 @@ public class ArquillianTest {
     // @AwaitRoute //It requires a GET endpoint, but mock-server has none by default
     @RouteURL("mockserverroute")
     URL mockserver;
+
+    @Named("mariadb")
+    @ArquillianResource
+    Pod mariadb;
+
 
     //Not used, these are here only for example:
     @ArquillianResource //Arquillian namespace: session.getNamespace() or oc.getNamespace()
@@ -101,25 +110,7 @@ public class ArquillianTest {
             //log.info("error: {}", IOUtils.toString(mysql.getErrorStream()));
 
             // Prepare database
-            // Run command in container using OpenShiftClient java client - run sql in mysql
-            log.info("Sql load - start");
-            final CountDownLatch latch = new CountDownLatch(1);
-            OutputStream execOut = new ByteArrayOutputStream();
-            OutputStream execErr = new ByteArrayOutputStream();
-            ExecWatch exec = oc.pods().withName("mariadb")
-                    .readingInput(this.getClass().getClassLoader().getResourceAsStream("sql/sql-load.sql"))
-                    .writingOutput(execOut)
-                    .writingError(execErr)
-                    //.withTTY() //Optional
-                    .usingListener(createCountDownListener(latch))
-                    .exec("/opt/rh/rh-mariadb102/root/usr/bin/mysql","-u", "myuser", "-pmypassword", "-h", "127.0.0.1", "testdb")
-                    ;
-            if (!latch.await(20, TimeUnit.SECONDS)) {
-                throw new Exception("Exec timeout");
-            }
-            log.info("Exec out: {}", ((ByteArrayOutputStream) execOut).toString());
-            log.info("Exec err: {}", ((ByteArrayOutputStream) execErr).toString());
-            log.info("Sql load - end");
+            loadSql();
 
 
             //Prepare MockServer response for test
@@ -148,6 +139,7 @@ public class ArquillianTest {
         }
     }
 
+    //Happy path
     @Test
     public void testSucc() throws Exception {
 
@@ -196,6 +188,78 @@ public class ArquillianTest {
         assertEquals("1 Test St", JsonPath.read(response, "$.address.address"));
         assertEquals("T001", JsonPath.read(response, "$.address.zip"));
 
+    }
+
+    //Test verifies that the retry works in case of temporary database downtime
+    @Test
+    public void shutDownMariadb() throws Exception{
+        //Delete mariadb pod
+        log.info("mariadb: {}", oc.pods().withName("mariadb").get()); //not null
+        log.info("Delete mariadb pod.");
+        assertTrue( oc.pods().withName(mariadb.getMetadata().getName()).delete() );
+        Awaitility.await().atMost(30,TimeUnit.SECONDS).until(()->oc.pods().withName("mariadb").get()==null);
+
+        //The port-foward url has http schema, but it's actually the amq 61616 port
+        String brokerUrl = "tcp://"+ amqsvcUrl.getHost()+":"+ amqsvcUrl.getPort();
+        log.info("brokerUrl: {}",brokerUrl);
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("test","secret",brokerUrl);
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+
+        //Send test message and receive outcome
+        jmsTemplate.convertAndSend("user.in", "{\"email\":\"testSucc@test.com\"}");
+
+        //Wait a sec to make sure message fails
+        Thread.sleep(3000);
+
+        //Create mariadb Pod and load sql
+        log.info("Create mariadb pod.");
+        ObjectMeta emptyObjectMeta = new ObjectMeta();
+        emptyObjectMeta.setName(mariadb.getMetadata().getName());
+        emptyObjectMeta.setLabels(mariadb.getMetadata().getLabels());
+        mariadb.setMetadata(emptyObjectMeta);
+        mariadb.setStatus(null);
+        Pod newPod = oc.pods().create(mariadb);
+        log.info("mariadb: {}", oc.pods().withName("mariadb").get());
+        Awaitility.await().atMost(30,TimeUnit.SECONDS).until(()->oc.pods().withName("mariadb").isReady());
+        //Recreate table, load data
+        loadSql();
+
+        //Receive response message
+        TextMessage message = (TextMessage) jmsTemplate.receive("user.out");
+        String response = message.getText();
+
+        log.info("Response: {}",response);
+
+        //Asserts
+        assertEquals("testSucc@test.com", JsonPath.read(response, "$.email"));
+        assertEquals("5551234567", JsonPath.read(response, "$.phone"));
+        assertEquals("Test State", JsonPath.read(response, "$.address.state"));
+        assertEquals("Test City", JsonPath.read(response, "$.address.city"));
+        assertEquals("1 Test St", JsonPath.read(response, "$.address.address"));
+        assertEquals("T001", JsonPath.read(response, "$.address.zip"));
+
+    }
+
+    private void loadSql() throws Exception{
+        // Run command in container using OpenShiftClient java client - run sql in mysql
+        log.info("Sql load - start");
+        final CountDownLatch latch = new CountDownLatch(1);
+        OutputStream execOut = new ByteArrayOutputStream();
+        OutputStream execErr = new ByteArrayOutputStream();
+        ExecWatch exec = oc.pods().withName("mariadb")
+                .readingInput(this.getClass().getClassLoader().getResourceAsStream("sql/sql-load.sql"))
+                .writingOutput(execOut)
+                .writingError(execErr)
+                //.withTTY() //Optional
+                .usingListener(createCountDownListener(latch))
+                .exec("/opt/rh/rh-mariadb102/root/usr/bin/mysql","-u", "myuser", "-pmypassword", "-h", "127.0.0.1", "testdb")
+                ;
+        if (!latch.await(20, TimeUnit.SECONDS)) {
+            throw new Exception("Exec timeout");
+        }
+        log.info("Exec out: {}", ((ByteArrayOutputStream) execOut).toString());
+        log.info("Exec err: {}", ((ByteArrayOutputStream) execErr).toString());
+        log.info("Sql load - end");
     }
 
     //Helper method for OpenShiftClient exec()
